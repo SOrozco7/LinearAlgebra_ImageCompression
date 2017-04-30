375950c91ed4730f861c4d2892867a79b021d834
Salvador Orozco Villalever <salvador@orozco.in>
2017-04-30 14:06:30 -0500

    class Transformation was fixed

--- IFS.java
+++ IFS.java
@@ -7,10 +7,20 @@ public class IFS{
 
 	/*
 		Constructor: 
-		Método add para hacerles push a las transformaciones y las p's (por parejas) en una array list
+		Método add para insertar a las transformaciones y las p's (por parejas) en una array list
 		método executeTransformations: recibe una imagen y devuelve otra
 		método reverseExecuteTransformations: recibe una imagen y devuelve otra
 	*/
 
+	private ArrayList<Transformation> totalTransformations;
 
+	public IFS(){
+
+		totalTransformations = new ArrayList<Transformation>();
+	}
+
+	public void addTransformation(Transformation t){
+
+		totalTransformations.add(t);
+	}
 }
